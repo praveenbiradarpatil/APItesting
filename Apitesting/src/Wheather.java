a52d130eb4ec1d0447b14b9793f2837e26958f00
praveenbiradarpatil <56438834+praveenbiradarpatil@users.noreply.github.com>
2019-10-23 15:36:01 +0530

    Add files via upload

---
+++ Apitesting/src/Wheather.java
@@ -0,0 +1,35 @@
+import static io.restassured.RestAssured.given;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class Wheather {
+
+	
+	String custermerKey="2Xtd4ILUtinfACHehAvCNcbjW";
+	String cunsumerSecret="pIHDmJy9fgKlLVuAgG4AuP5rRkaOy6TtWqxLqjVeJ2MywdcGJp";
+	String Token="839104214-h1Kaf8iJW2FAyXzNCZNicI1a4Xb8BMw7ZkFKGzmJ";
+	String TokenSecret="PTvXawkmrK3EZWeLSSmqtD7hisogRC3nqG48FYzNcLfQy";
+	
+
+	
+	@Test
+	public void searchtweet()
+	{
+		RestAssured.baseURI="https://api.twitter.com/1.1/search/";
+		Response res=given().auth().oauth(custermerKey, cunsumerSecret, Token, TokenSecret).
+		queryParam("q","Bangalore Weather").
+		when().get("tweets.json").then().extract().response();
+		
+		
+		String responce=res.asString();//to convert into string
+		System.out.println(responce);
+		
+		
+	
+		
+	
+}
+}
